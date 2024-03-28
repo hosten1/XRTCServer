2431db703836ad373c04e62bd4635492e13e5c17
luoyongmeng <hosten_lym@163.com>
2024-03-28 15:56:39 +0800

    Update main.go

--- LYMSDK/signaling/src/main.go
+++ LYMSDK/signaling/src/main.go
@@ -5,12 +5,15 @@ package main
 import (
 	"fmt"
 	"test/src/framework"
+	"test/src/glog"
 )
 func main(){
+	flag.Parse()
 	errInit := framework.Init()
 	if errInit != nil {
 		panic(errInit)
 	}
+	glog.info("Init success")
 	errHttp := framework.StartHttp()
 	if errHttp != nil {
 		panic(errHttp)
