e800e50a2784390d45e06986effadf649e98b23c
lym <hosten_lym@163.com>
2024-03-29 18:30:05 +0800

    push请求go解析

--- LYMSDK/signaling/src/action/push.go
+++ LYMSDK/signaling/src/action/push.go
@@ -1,9 +1,10 @@
 package action
 
 import (
-	// "fmt"
-	// "html/template"
+	"fmt"
+	"strconv"
 	"test/src/framework"
+	"test/src/commonErrors"
 	"net/http"
 )
 
@@ -15,5 +16,53 @@ func NewPushAction()  *PushAction {
 }
 
 func (*PushAction) Execute(w http.ResponseWriter, cr *framework.CommonRequest){
+  r := cr.R
+  //uid
+  var strUid string
+  if values , ok := r.Form["uid"]; ok {
+	strUid = values[0]
+  }
+  uid,err := strconv.ParseUint(strUid,10,64)
+  if err != nil || uid <= 0 {
+	cerr := commonErrors.New(commonErrors.ParamErr,"parse uid error:"+err.Error())
+	writeJsonErrorResponse(cerr,w,cr)
+	return
+	
+  }
 
+  //streamName
+  var streamName string
+  if values , ok := r.Form["streamName"]; ok {
+	streamName = values[0]
+  }
+  if "" == streamName {
+	cerr := commonErrors.New(commonErrors.ParamErr," streamName is null!!")
+	writeJsonErrorResponse(cerr,w,cr)
+	return
+  }
+
+  //audio
+  var audioStr string
+  var audio int
+  if values , ok := r.Form["audio"]; ok {
+	audioStr = values[0]
+  }
+  if "" == audioStr || "0" == audioStr {
+	audio = 0;
+  }else{
+	audio = 1;
+  }
+
+  //streamName
+  var videoStr string
+  var video int
+  if values , ok := r.Form["video"]; ok {
+	videoStr = values[0]
+  }
+  if "" == videoStr || "0" == videoStr {
+	video = 0;
+  }else{
+	video = 1;
+  }
+  fmt.Println(uid,streamName,audio,video)
 }
